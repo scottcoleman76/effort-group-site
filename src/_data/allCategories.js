81560a79e807107f75293a5c108b34330459fa9a
scottcoleman76 <64758617+scottcoleman76@users.noreply.github.com>
2026-07-16 08:47:43 -0500

    Update allCategories.js

--- src/_data/allCategories.js
+++ src/_data/allCategories.js
@@ -4,7 +4,10 @@ module.exports = () => {
   const all = articles();
   const catSet = new Set();
   all.forEach((a) => {
-    (a.category || []).forEach((c) => catSet.add(c));
+    (a.category || []).forEach((c) => {
+      const val = typeof c === "string" ? c : c.value;
+      if (val) catSet.add(val);
+    });
   });
   return Array.from(catSet).sort();
 };
